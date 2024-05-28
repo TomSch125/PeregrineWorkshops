f4f28ae210c839cdc17cdb110bdaf85c7f90d179
TomSch125 <schofield.tom@hotmail.com>
2024-05-28 11:30:40 +0100

    hangman completed

---
+++ Workshop5/Hangman/GameObj.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class GameObj
+    {
+        String[] Pics;
+        String[] Words;
+        string target;
+        int guesses = 0;
+        List<char> current = new List<char>();
+
+        public GameObj(string[] pics, string[] words)
+        {
+            Pics = pics;
+            Words = words;
+            Random rnd = new Random();
+            this.target = words[rnd.Next(1, words.Length)];
+            this.guesses = 0;
+            for (int i = 0; i < target.Length; i++)
+            {
+                current.Add('?');
+            }
+        }
+
+        public void gameLoop() {
+
+            Console.WriteLine(target);
+
+            while (guesses<7)
+            {   
+                printCurrent();
+                Console.WriteLine("\ninput guess:");
+                string input = Console.ReadLine().ToLower();
+                checkTarget(input);
+                if (!current.Contains('?')) {
+                    Console.WriteLine("Word Found !!!!!");
+                    return;
+                }
+
+                printHangman();
+
+                guesses++;
+            }
+
+            Console.WriteLine("Failed to find the word");
+
+
+        }
+
+        private void checkTarget(string guess)
+        {
+            if (guess.Length > target.Length) {
+                guess = guess.Substring(0, target.Length);
+            }
+
+            char[] targetChrs = target.ToCharArray();
+            char[] guessChrs = guess.ToCharArray();
+            for (int i = 0; i < guessChrs.Length; i++) { 
+                if (targetChrs[i] == guessChrs[i]) {
+                    current[i] = guessChrs[i];
+                }
+            }
+
+        }
+        private void printCurrent()
+        {
+            string res = "";
+            foreach (char c in current)
+            {
+                if (c == '?')
+                {
+                    res = res + '_';
+                }
+                else {
+                    res = res + c;
+                }
+                res = res + ' ';
+            }
+
+            Console.WriteLine(res);
+        }
+
+        private void printHangman() {
+            Console.WriteLine('\n'+Pics[guesses]);
+        }
+    }
+}
---
+++ Workshop5/Hangman/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Hangman
+{
+    internal class Program
+    {
+
+        static String[] Pics = [@"  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========", @"  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========", @"  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========", @"  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========", @"  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========", @"  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========", @"  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+========="];
+
+        static String allWords = "ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolfwombat zebra";
+        static String[] words = allWords.Split(new char[] { ' ' });
+
+        static void Main(string[] args)
+        {
+            GameObj game = new GameObj(Pics, words);
+            game.gameLoop();
+            
+        }
+
+    }
+}
