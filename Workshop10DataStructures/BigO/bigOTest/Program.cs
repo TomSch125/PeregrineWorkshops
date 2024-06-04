f6f78db6e9fe6052514fcf41bc7bab09af0fb6e8
TomSch125 <schofield.tom@hotmail.com>
2024-06-04 13:23:47 +0100

    trees done

---
+++ Workshop10DataStructures/BigO/bigOTest/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace bigOTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(fun2(10));
+        }
+
+        public static int fun1(int n) {
+            int i, j, m = 0;
+            int ran = 0;
+            for (i = n; i > 0; i = i/2)
+            {
+                for (j = 0; j < i; j++)
+                {
+                    m = m + 1;
+                }
+            }
+            return m;
+        }
+
+        public static int fun2(int n) { 
+            int i, j, m = 0;
+
+            for (i = 0; i < n; i++) {
+                for (j = i; j < n; j++) {
+                    m = m + 1;
+                }
+            }
+
+            return m;
+        
+        }
+    }
+}
---
+++ Workshop10DataStructures/Trees/ShuntingYard/ShuntingYard/ShuntingYard/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShuntingYardNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Reference
+            / https://rosettacode.org/wiki/Parsing/Shunting-yard_algorithm#C.23
+            / https://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#C.23
+            /
+            */
+
+            //ShuntingYard() algorithm  - must have a space between each token;
+            string infix = " 3 + 2 * 3 + 4 + 2 * ( ( 3 + 2 ) - ( 4 - 2 ) ) ";
+            //convert to reverse polish notation
+            String rpnnotation = infix.ToPostfix();
+            // calculate reverse polish post fix notation
+            Decimal answer = RPNcs.CalculateRPN(rpnnotation);
+            Console.WriteLine("answer is : {0}", answer);
+            Console.ReadLine();
+        }
+
+        
+    }   
+}
